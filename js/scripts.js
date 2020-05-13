c2acb09f447dad27eefeaf21be8279e14f7a297e
raulbarriga <barrigaraulaug@gmail.com>
2020-05-12 20:28:25 -0700

    Adding the stuff I've been working on in full-screen view. Not much. The search box is good & the filter/sort row's also good. Gonna branch out to try and handle the filter menu & property columns which aren't displaying in rows but instead are showing as a whole column. Gonna try adding rows to keep it all lined properly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 
 
+// Filter Toggle Button
 $(function(){
     $("#filter-btn").click(function(){
       $("#filter").toggle();
     });
   });
 
+  // Property Type Button Dropdown Remains Open on Checkbox Selection
 $('.dropdown-menu').click(function(e) {
     e.stopPropagation();
 });
