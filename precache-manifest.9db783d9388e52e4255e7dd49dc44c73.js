713fe86a2a0f65da440ca7d57c3f8ae4f948d4bf
Raul Barriga <barrigaraulaug@gmail.com>
2022-06-10 10:49:08 -0700

    Updates

---
+++ precache-manifest.9db783d9388e52e4255e7dd49dc44c73.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650ca2a7c07ef33e5c5d88e49e0823ef",
+    "url": "/bootcamp-portfolio-project/index.html"
+  },
+  {
+    "revision": "4f66dd526a621f805a82",
+    "url": "/bootcamp-portfolio-project/static/css/2.27d93faf.chunk.css"
+  },
+  {
+    "revision": "daddbc75a7e42a737f83",
+    "url": "/bootcamp-portfolio-project/static/css/main.fce444b1.chunk.css"
+  },
+  {
+    "revision": "4f66dd526a621f805a82",
+    "url": "/bootcamp-portfolio-project/static/js/2.d8704c3b.chunk.js"
+  },
+  {
+    "revision": "3ecfdbc13754c9d84b5378af08964017",
+    "url": "/bootcamp-portfolio-project/static/js/2.d8704c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daddbc75a7e42a737f83",
+    "url": "/bootcamp-portfolio-project/static/js/main.cbc79c9e.chunk.js"
+  },
+  {
+    "revision": "a8e601b472cc79ebe0f0",
+    "url": "/bootcamp-portfolio-project/static/js/runtime-main.48463009.js"
+  },
+  {
+    "revision": "436fa48e30e9ad5ecbec555bc04e64a4",
+    "url": "/bootcamp-portfolio-project/static/media/app-store-logo.436fa48e.png"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/bootcamp-portfolio-project/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/bootcamp-portfolio-project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/bootcamp-portfolio-project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/bootcamp-portfolio-project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/bootcamp-portfolio-project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/bootcamp-portfolio-project/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/bootcamp-portfolio-project/static/media/google-play-badge.db9b21a1.png"
+  }
+]);
