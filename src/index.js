0c11de085671b8b299f2b66348dda0b71634ba84
Raul Barriga <barrigaraulaug@gmail.com>
2022-07-03 12:51:44 -0700

    Removing any extra css code from the old way it use to look.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "jquery/dist/jquery.min.js";
 import "popper.js/dist/umd/popper.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import "photoswipe/dist/photoswipe.css";
-import "photoswipe/dist/default-skin/default-skin.css";
 //import "theia-sticky-sidebar/dist/ResizeSensor.min.js";
 //import "theia-sticky-sidebar/dist/theia-sticky-sidebar.min.js";
 import "font-awesome/css/font-awesome.css";
